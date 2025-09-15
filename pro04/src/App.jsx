b2a1a33a6f150b1a3198b8c87c0bd1337801d729
허윤 <huryoun5022@naver.com>
2025-09-15 21:45:46 +0900

    feat(pro04) : redux 사용 시작

--- pro04/src/App.jsx
+++ pro04/src/App.jsx
@@ -2,15 +2,33 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch, connect } from "react-redux";
+
+function reducer(cuurentState, action) {
+  if (cuurentState === undefined) {
+    return {
+      number: 1,
+    };
+  }
+  const newState = { ...cuurentState };
+  return newState;
+}
+
+const stroe = createStore(reducer);
 
 export default function App() {
   const [number, setNumber] = useState(1);
-
   return (
     <>
       <div id="container">
-        <h1>Root</h1>
-        <Left1 number={number}></Left1>
+        <h1>Root : {number}</h1>
+        <div id="grid">
+          <Provider store={stroe}>
+            <Left1></Left1>
+            <Right1></Right1>
+          </Provider>
+        </div>
       </div>
     </>
   );
@@ -19,23 +37,48 @@ export default function App() {
 function Left1(props) {
   return (
     <div>
-      <h1>Left1 : {props.number}</h1>
-      <Left2 number={props.number}></Left2>
+      <h1>Left1 : </h1>
+      <Left2></Left2>
     </div>
   );
 }
 function Left2(props) {
   return (
     <div>
-      <h1>Left2 : {props.number}</h1>
-      <Left3 number={props.number}></Left3>
+      <h1>Left2 : </h1>
+      <Left3></Left3>
     </div>
   );
 }
 function Left3(props) {
+  const number = useSelector((state) => state.number);
   return (
     <div>
-      <h1>Left3 : {props.number}</h1>
+      <h1>Left3 : {number}</h1>
+    </div>
+  );
+}
+function Right1(props) {
+  return (
+    <div>
+      <h1>Right1</h1>
+      <Right2></Right2>
+    </div>
+  );
+}
+function Right2(props) {
+  return (
+    <div>
+      <h1>Right2</h1>
+      <Right3></Right3>
+    </div>
+  );
+}
+function Right3(props) {
+  return (
+    <div>
+      <h1>Right3</h1>
+      <input type="button" value="+" onClick={() => {}} />
     </div>
   );
 }
